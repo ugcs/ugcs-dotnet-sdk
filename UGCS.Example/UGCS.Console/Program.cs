f496362d3e015d2e288dbe05141e7760dc5d08ac
UGCS-sdk <sdk@ugcs.com>
2016-11-11 17:00:03 +0200

    console app fix.

--- UGCS.Example/UGCS.Console/Program.cs
+++ UGCS.Example/UGCS.Console/Program.cs
@@ -10,111 +10,111 @@ using ProtoBuf;
 
 namespace UGCS.Console
 {
-    public class TelemetryListenerTest : IMessageListener
-    {
-        public void MessageReceived(IExtensible message)
-        {
-            int x;
-            var msg = (Notification)message;
-            String s = "";
-            foreach (Telemetry t in msg.Event.TelemetryEvent.Telemetry)
-            {
-                if (t.Vehicle.Id != 1)
-                {
-                    continue;
-                }
-                System.Console.WriteLine(t.Vehicle.Id + ": " + t.Type.ToString() + " - "+ t.Value);
-                switch (t.Type)
-                {
-                    case TelemetryType.TT_LATITUDE:
-                        x = 1;
-                        break;
-                    case TelemetryType.TT_LONGITUDE:
-                        x = 1;
-                        break;
-                    case TelemetryType.TT_LATITUDE_GPS:
-                        x = 1;
-                        break;
-                    case TelemetryType.TT_LONGITUDE_GPS:
-                        x = 1;
-                        break;
-                }
-            }
-        }
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
 
-            var Listener = new TelemetryListenerTest();
-            EventSubscriptionWrapper esw = new EventSubscriptionWrapper();
-            esw.TelemetrySubscription = new TelemetrySubscription();
-
+            //Connect
             TcpClient tcpClient = new TcpClient("localhost", 3334);
             MessageSender messageSender = new MessageSender(tcpClient.Session);
             MessageReceiver messageReceiver = new MessageReceiver(tcpClient.Session);
-            messageReceiver.AddListener(-1, Listener);
             MessageExecutor messageExecutor = new MessageExecutor(messageSender, messageReceiver, new InstantTaskScheduler());
             messageExecutor.Configuration.DefaultTimeout = 10000;
+            var notificationListener = new NotificationListener();
+            messageReceiver.AddListener(-1, notificationListener);
+
+            //auth
             AuthorizeHciRequest request = new AuthorizeHciRequest();
             request.ClientId = -1;
-            LoginRequest loginRequest = new LoginRequest();
-            loginRequest.UserLogin = "admin";
-            loginRequest.UserPassword = "admin";
-
+            request.Locale = "en-US";
             var future = messageExecutor.Submit<AuthorizeHciResponse>(request);
             future.Wait();
             AuthorizeHciResponse AuthorizeHciResponse = future.Value;
+            int clientId = AuthorizeHciResponse.ClientId;
 
-            loginRequest.ClientId = AuthorizeHciResponse.ClientId;
-            LoginResponse LoginResponce = messageExecutor.Submit<LoginResponse>(loginRequest).Value;
+            //login
+            LoginRequest loginRequest = new LoginRequest();
+            loginRequest.UserLogin = "admin";
+            loginRequest.UserPassword = "admin";
+            loginRequest.ClientId = clientId;
+            var loginResponcetask = messageExecutor.Submit<LoginResponse>(loginRequest);
+            loginResponcetask.Wait();
 
-
-            System.Console.WriteLine("Telemetry subscribed");
-
-            GetObjectRequest GetObjectRequest = new GetObjectRequest()
+            //Get all vehicles
+            GetObjectListRequest getObjectListRequest = new GetObjectListRequest()
             {
-                ClientId = AuthorizeHciResponse.ClientId,
-                ObjectId = 1,
-                ObjectType = "Route",
+                ClientId = clientId,
+                ObjectType = "Vehicle",
                 RefreshDependencies = true
             };
-
-            GetObjectRequest.RefreshExcludes.Add("Avatar");
-            GetObjectRequest.RefreshExcludes.Add("Vehicle");
-            GetObjectRequest.RefreshExcludes.Add("PayloadProfile");
-            GetObjectRequest.RefreshExcludes.Add("Mission");
-
-            var task = messageExecutor.Submit<GetObjectResponse>(GetObjectRequest);
+            getObjectListRequest.RefreshExcludes.Add("Avatar");
+            getObjectListRequest.RefreshExcludes.Add("PayloadProfile");
+            getObjectListRequest.RefreshExcludes.Add("Route");
+            var task = messageExecutor.Submit<GetObjectListResponse>(getObjectListRequest);
             task.Wait();
 
-            Route route = task.Value.Object.Route;
-
-            ProcessRouteRequest ProcessRouteRequest = new ProcessRouteRequest
+            var list = task.Value;
+            foreach (var v in list.Objects)
             {
-                ClientId = AuthorizeHciResponse.ClientId,
-                Route = route,
-            };
-
-            MessageFuture<ProcessRouteResponse> ProcessRouteResponse = messageExecutor.Submit<ProcessRouteResponse>(ProcessRouteRequest);
-            ProcessRouteResponse.Wait();
-
-            if (ProcessRouteResponse.Exception != null || ProcessRouteResponse.Value == null)
-            {
-                throw new Exception("Calculate error: " + ProcessRouteResponse.Exception.Message);
+                System.Console.WriteLine(string.Format("name: {0}; id: {1}; type: {2}",
+                       v.Vehicle.Name, v.Vehicle.Id, v.Vehicle.Type.ToString()));
             }
+            Vehicle vehicle = task.Value.Objects.FirstOrDefault().Vehicle;
 
-            System.Console.WriteLine("Route calculated");
+            //update vehicle object
+            CreateOrUpdateObjectRequest createOrUpdateObjectRequest = new CreateOrUpdateObjectRequest()
+            {
+                ClientId = clientId,
+                Object = new DomainObjectWrapper().Put(vehicle, "Vehicle"),
+                WithComposites = true,
+                ObjectType = "Vehicle",
+                AcquireLock = false
+            };
+            var createOrUpdateObjectResponseTask = messageExecutor.Submit<CreateOrUpdateObjectResponse>(createOrUpdateObjectRequest);
+            createOrUpdateObjectResponseTask.Wait();
 
-
+            //Vehicle notification subscription
+            var eventSubscriptionWrapper = new EventSubscriptionWrapper();
+            eventSubscriptionWrapper.ObjectModificationSubscription = new ObjectModificationSubscription();
+            eventSubscriptionWrapper.ObjectModificationSubscription.ObjectId = vehicle.Id;
+            eventSubscriptionWrapper.ObjectModificationSubscription.ObjectType = "Vehicle";
             SubscribeEventRequest requestEvent = new SubscribeEventRequest();
-            requestEvent.ClientId = AuthorizeHciResponse.ClientId;
+            requestEvent.ClientId = clientId;
+            requestEvent.Subscription = eventSubscriptionWrapper;
+            var responce = messageExecutor.Submit<SubscribeEventResponse>(requestEvent);
+            responce.Wait();
+            var subscribeEventResponse = responce.Value;
+            SubscriptionToken st = new SubscriptionToken(subscribeEventResponse.SubscriptionId, (
+                (notification) =>
+                {
+                    //Vehicle notification
+                }
+            ), eventSubscriptionWrapper);
+            notificationListener.AddSubscription(st);
 
-            requestEvent.Subscription = esw;
-            var tm = messageExecutor.Submit<SubscribeEventResponse>(requestEvent);
-            tm.Wait();
+            //TelemetrySubscription
+            var telemetrySubscriptionWrapper = new EventSubscriptionWrapper();
+            telemetrySubscriptionWrapper.TelemetrySubscription = new TelemetrySubscription();
+            SubscribeEventRequest requestTelemetryEvent = new SubscribeEventRequest();
+            requestTelemetryEvent.ClientId = clientId;
+            requestTelemetryEvent.Subscription = telemetrySubscriptionWrapper;
+            var responceTelemetry = messageExecutor.Submit<SubscribeEventResponse>(requestTelemetryEvent);
+            responceTelemetry.Wait();
+            var subscribeEventResponseTelemetry = responceTelemetry.Value;
+            SubscriptionToken stTelemetry = new SubscriptionToken(subscribeEventResponseTelemetry.SubscriptionId, (
+                (notification) =>
+                {
+                    foreach (var t in notification.Event.TelemetryEvent.Telemetry)
+                    {
+                        System.Console.WriteLine("Vehicle id: {0} Type: {1} Value {2}", t.Vehicle.Id, t.Type.ToString(), t.Value);
+                    }
+                }
+            ), telemetrySubscriptionWrapper);
+            notificationListener.AddSubscription(stTelemetry);
+
+
 
             System.Console.ReadKey();
             Environment.Exit(1);
